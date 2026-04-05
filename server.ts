017b101f15a9d7fa8617d8f358ccb43fee2e89d8
Phu Quoc <nguyenphuquoc889@gmail.com>
2026-04-05 22:50:28 +0700

    newss

--- server.ts
+++ server.ts
@@ -115,3 +115,4 @@ const startServer = async () => {
 
 startServer();
 export { sequelize };
+export default app;
--- server/config/database.ts
+++ server/config/database.ts
@@ -1,9 +1,21 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize("database_development", "root", "123456", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "railway", 
+  process.env.DB_USER || "root", 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      connectTimeout: 60000 // Tăng thời gian chờ kết nối
+    }
+  }
+);
 
 export default sequelize;
