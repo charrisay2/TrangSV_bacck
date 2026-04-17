4ef74c43e53a23489e236843082199a79daee104
Phu Quoc <nguyenphuquoc889@gmail.com>
2026-04-17 20:14:26 +0700

    data

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("database_development", "root", "123456", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
-
+const sequelize = process.env.MYSQL_URL
+  ? new Sequelize(process.env.MYSQL_URL, {
+      dialect: "mysql",
+      logging: false,
+    })
+  : new Sequelize("database_development", "root", "123456", {
+      host: "127.0.0.1",
+      dialect: "mysql",
+      logging: false,
+    });
 export default sequelize;
