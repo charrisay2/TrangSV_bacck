2be2d6a9db7524c09f144365d2f2471b63a3f67d
Phu Quoc <nguyenphuquoc889@gmail.com>
2026-07-23 16:12:52 +0700

    Đuongan

--- uploadMiddleware.ts
+++ uploadMiddleware.ts
@@ -1,7 +1,6 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../SVHKback/server/config/cloudinary";
-
+import cloudinary from "./server/config/cloudinary";
 const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => ({
